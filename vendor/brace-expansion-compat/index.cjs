492a19102f301a5a6cfd391776f70262c302ef8c
Sergio Peschiera <speschiera@gmail.com>
2026-07-29 10:41:56 +0100

    Add brace-expansion compatibility entrypoint

---
+++ vendor/brace-expansion-compat/index.cjs
@@ -0,0 +1,8 @@
+'use strict';
+
+const modern = require('brace-expansion-modern');
+
+module.exports = modern.expand;
+module.exports.expand = modern.expand;
+module.exports.EXPANSION_MAX = modern.EXPANSION_MAX;
+module.exports.EXPANSION_MAX_LENGTH = modern.EXPANSION_MAX_LENGTH;
